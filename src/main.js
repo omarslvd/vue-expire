c04f339967d3f974cc19ab76f89662b7b7a1c781
Omar Silva <omarsilva@outlook.com>
2020-07-29 00:30:56 -0500

    se agrega kendo ui

--- src/main.js
+++ src/main.js
@@ -3,8 +3,12 @@ import 'mutationobserver-shim'
 import Vue from 'vue'
 import './plugins/bootstrap-vue'
 import App from './App.vue'
+import '@progress/kendo-ui'
+import '@progress/kendo-theme-bootstrap/dist/all.css'
+import { DatePicker } from '@progress/kendo-vue-dateinputs'
 
 Vue.config.productionTip = false
+Vue.component('datepicker', DatePicker)
 
 new Vue({
   render: h => h(App),
