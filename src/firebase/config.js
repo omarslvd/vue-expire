ae1220963de269bc09a21f8a90274a25297d39fa
Omar Silva <omarsilva@outlook.com>
2020-08-19 23:36:45 -0500

    Se agrega firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,19 @@
+import firebase from "firebase";
+import "firebase/firestore";
+
+var firebaseConfig = {
+  apiKey: "### FIREBASE API KEY ###",
+  authDomain: "### FIREBASE AUTH DOMAIN ###",
+  databaseURL: "### CLOUD FIRESTORE PROJECT ID ###",
+  projectId: "### CLOUD FIRESTORE PROJECT ID ###",
+  storageBucket: "### CLOUD FIRESTORE PROJECT ID ###",
+  messagingSenderId: "### CLOUD FIRESTORE PROJECT ID ###",
+  appId: "### CLOUD FIRESTORE PROJECT ID ###",
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+var db = firebase.firestore();
+
+export { db };
